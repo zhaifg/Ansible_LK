9c217d984b39d465c134afd513b199cbd3e2cb30
zhaifg <zhaifg@longekysoft.cn>
2015-08-21 09:45:48 +0000

    bbbb

---
+++ common/files/InPubKey.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#USER={{ remote_user  }}
+
+
+if [[  -d /root/.ssh  ]];then 
+    cat /tmp/id_rsa.pub >> /root/.ssh/known_hosts
+    chmod 400 /root/.ssh/known_hosts
+  
+else
+    mkdir /root/.ssh && chmod -R 700 /root/.ssh
+    cat /tmp/id_rsa.pub >> /root/.ssh/known_hosts
+    chmod 400 /root/.ssh/known_hosts
+    
+fi
