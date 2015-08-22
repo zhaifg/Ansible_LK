91a9ff1314c177c45e9a982512c48e0031f572e2
zhaifg <zhaifg@longekysoft.cn>
2015-08-22 01:58:19 +0000

    bugfix

--- common/files/InPubKey.sh
+++ common/files/InPubKey.sh
@@ -5,7 +5,7 @@
 
 if [[  -d /root/.ssh  ]];then 
     cat /tmp/id_rsa.pub >> /root/.ssh/authorized_keys
-    chmod 400 /root/.ssh/known_hosts
+    chmod 400 /root/.ssh/authorized_keys
   
 else
     mkdir /root/.ssh && chmod -R 700 /root/.ssh
