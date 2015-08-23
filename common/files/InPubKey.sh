a02400d10194e5ad7e927d738a9be15852164c11
zhaifg <zhaifg@longekysoft.cn>
2015-08-23 07:22:43 +0000

    wangle

--- common/files/InPubKey.sh
+++ common/files/InPubKey.sh
@@ -4,12 +4,12 @@
 
 
 if [[  -d /root/.ssh  ]];then 
-    cat /tmp/id_rsa.pub >> /root/.ssh/authorized_keys
+    cat /tmp/id_rsa_manager_root_prv.pub >> /root/.ssh/authorized_keys
     chmod 400 /root/.ssh/authorized_keys
   
 else
     mkdir /root/.ssh && chmod -R 700 /root/.ssh
-    cat /tmp/id_rsa.pub >> /root/.ssh/authorized_keys 
+    cat /tmp/id_rsa_manager_root_prv.pub >> /root/.ssh/authorized_keys 
     chmod 400 /root/.ssh/authorized_keys 
     
 fi
